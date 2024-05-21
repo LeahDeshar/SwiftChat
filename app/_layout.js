56c604aefe888082588b12e1987aaf68e5e06780
Leah Deshar <leah.desar@gmail.com>
2024-05-21 16:53:53 +0545

    auth

--- app/_layout.js
+++ app/_layout.js
@@ -1,17 +1,21 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
-import { Slot, useSegments } from "expo-router";
-import { AuthContextProvider, useAuth } from "../.expo/context/authContext";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { AuthContextProvider, useAuth } from "../context/authContext";
 import "../global.css";
 
 const MainLayout = () => {
   const { isAuthenticated } = useAuth();
   const segments = useSegments();
-
+  const router = useRouter();
   useEffect(() => {
-    // if (!isAuthenticated) {
-    //   segments.replace("signIn");
-    // }
+    if (typeof isAuthenticated == "undefined") return;
+    const inApp = segments[0] == "(app)";
+    if (isAuthenticated && !inApp) {
+      router.replace("home");
+    } else if (isAuthenticated == false) {
+      router.replace("signIn");
+    }
   }, [isAuthenticated]);
   return <Slot />;
 };
---
+++ context/authContext.js
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthentication] = useState(undefined);
+
+  useEffect(() => {}, []);
+
+  const login = async (email, password) => {
+    try {
+    } catch (error) {}
+  };
+
+  const logout = async () => {
+    try {
+    } catch (error) {}
+  };
+
+  const register = async (email, password, profileUrl) => {
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+        register,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const value = useContext(AuthContext);
+  if (!value) {
+    throw new Error("useAuth must be used within an AuthProvider.");
+  }
+  return value;
+};
