a268dcb12a467b5c13ac6bb1c5647bc5f52911e2
Leah Deshar <leah.desar@gmail.com>
2024-05-21 18:26:58 +0545

    auth- signin ui

--- app/signIn.js
+++ app/signIn.js
@@ -1,4 +1,12 @@
-import { View, Text, Image, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Alert,
+} from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
@@ -6,8 +14,21 @@ import {
   widthPercentageToDP as wp,
 } from "react-native-responsive-screen";
 import { Octicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { useRef } from "react";
 
 export default function signIn() {
+  const router = useRouter();
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const handleLogin = async () => {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert("Sign In", "Please fill all the fields");
+      return;
+    }
+  };
   return (
     <View className={"flex-1"}>
       <StatusBar style="dark" />
@@ -39,31 +60,63 @@ export default function signIn() {
               style={{ fontSize: hp(2) }}
               className={"flex-1 font-semibold text-neutral-700"}
               placeholder="Email Address"
+              onChangeText={(value) => (emailRef.current = value)}
               placeholderTextColor={"gray"}
             />
+          </View>
+          <View className="gap-3">
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-2xl"
+            >
+              <Octicons name="lock" size={hp(2.7)} color={"gray"} />
+              <TextInput
+                style={{ fontSize: hp(2) }}
+                className={"flex-1 font-semibold text-neutral-700"}
+                placeholder="Password"
+                placeholderTextColor={"gray"}
+                onChangeText={(value) => (passwordRef.current = value)}
+              />
+            </View>
+            <Text
+              style={{ fontSize: hp(1.8) }}
+              className="font-semibold text-right text-neutral-500"
+            >
+              Forgot Password ?
+            </Text>
           </View>
 
-          <View
-            style={{ height: hp(7) }}
-            className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-2xl"
+          <TouchableOpacity
+            onPress={handleLogin}
+            style={{ height: hp(6.5) }}
+            className="bg-indigo-500 rounded-xl justify-center items-center"
           >
-            <Octicons name="lock" size={hp(2.7)} color={"gray"} />
-            <TextInput
-              style={{ fontSize: hp(2) }}
-              className={"flex-1 font-semibold text-neutral-700"}
-              placeholder="Password"
-              placeholderTextColor={"gray"}
-            />
+            <Text
+              className="text-white font-bold tracking-wider"
+              style={{ fontSize: hp(2.7) }}
+            >
+              Sign In
+            </Text>
+          </TouchableOpacity>
+
+          <View className="flex-row justify-center">
+            <Text
+              style={{ fontSize: hp(1.8) }}
+              className="font-semibold text-neutral-500"
+            >
+              Don't have an account?
+            </Text>
+            <Pressable onPress={() => router.push("signUp")}>
+              <Text
+                style={{ fontSize: hp(1.8) }}
+                className="font-bold text-indigo-500"
+              >
+                Sign up
+              </Text>
+            </Pressable>
           </View>
-          <Text
-            style={{ fontSize: hp(1.8) }}
-            className="font-semibold text-right"
-          >
-            Forgot Password ?
-          </Text>
         </View>
       </View>
-      <Text>signIn</Text>
     </View>
   );
 }
