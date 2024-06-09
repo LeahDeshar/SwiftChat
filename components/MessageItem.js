0bc9e7adc095c39dfe58996a99ddcc653fd8560a
Leah Deshar <leah.desar@gmail.com>
2024-06-09 19:44:17 +0545

    Chat- send Message

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,12 +1,29 @@
 import { View, Text } from "react-native";
 import React from "react";
-
-const MessageItem = () => {
-  return (
-    <View>
-      <Text>MessageItem</Text>
-    </View>
-  );
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+const MessageItem = ({ message, currentUser }) => {
+  if (currentUser?.userId == message?.userId) {
+    return (
+      <View className={"flex-row justify-end mb-3 mr-3"}>
+        <View style={{ width: wp(80) }}>
+          <View className="flex self-end p-3 rounded-2xl bg-white border border-neutral-200">
+            <Text style={{ fontSize: hp(1.9) }}>{message?.text}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ width: wp(8) }} className="ml-3 mb-3">
+        <View className="flex self-start p-3 px-4 rounded-2xl bg-indigo-100 border border-indigo-200">
+          <Text style={{ fontSize: hp(1.9) }}>{message?.text}</Text>
+        </View>
+      </View>
+    );
+  }
 };
 
 export default MessageItem;
